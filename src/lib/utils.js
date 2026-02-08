0ccdc608799e7aa41b19ebbee4c65681def92e1e
TioElvis <elvis.veramijares@gmail.com>
2026-02-08 09:54:40 +0100

    feat: enhance onCancel function to clean up project directory if it exists

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,14 +1,20 @@
+import fs from "node:fs";
 import { promisify } from "node:util";
 import { exec } from "node:child_process";
 
 import chalk from "chalk";
 
+import { context } from "../main.js";
 import { Exception } from "./exception.js";
 
 const execPromise = promisify(exec);
 
 export function onCancel() {
   console.log(chalk.red("\nOperation cancelled."));
+  if (context.dest && fs.existsSync(context.dest)) {
+    fs.rmSync(context.dest, { recursive: true, force: true });
+    console.log(chalk.yellow("Cleaned up the created project directory."));
+  }
   process.exit(0);
 }
 
